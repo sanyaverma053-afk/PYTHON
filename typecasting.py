ae6158ce6eb4a9c69e23d9c3182b4fa6c67416c1
SANYA VERMA <sanyaverma053@gmail.com>
2026-03-12 00:25:02 +0530

    typecasting.py

---
+++ typecasting.py
@@ -0,0 +1,17 @@
+#typecasting
+
+a=input("enter a number")
+b=input("enter another number")
+print(a+b)
+print(a-b)
+print(a*b) 
+print(a/b)
+print(a//b)
+print(a**b)
+
+print(int(a)+int(b))
+print(int(a)-int(b))
+print(int(a)*int(b))
+print(int(a)/int(b))
+print(int(a)//int(b))
+print(int(a)**int(b))
