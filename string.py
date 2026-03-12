f6b3d502659f24b979c45d6eeadb1564739b735d
SANYA VERMA <sanyaverma053@gmail.com>
2026-03-13 00:26:07 +0530

    string.py

---
+++ string.py
@@ -0,0 +1,15 @@
+#string
+
+name="Harry"
+friend="rohan"
+anotherfriend='lovish'
+apple='''hi everyone
+sanya this side
+from up'''
+
+print("hello,"+name)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
