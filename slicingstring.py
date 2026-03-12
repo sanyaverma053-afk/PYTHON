58957c00702107a0057f14655ea40e8ec742122b
SANYA VERMA <sanyaverma053@gmail.com>
2026-03-13 00:26:58 +0530

    slicingstring.py

---
+++ slicingstring.py
@@ -0,0 +1,11 @@
+#slicing in string
+
+fruit="mango"
+mangoLen=len(fruit)
+print(mangoLen)
+print(fruit[0:4])
+print(fruit[1:4])
+print(fruit[:5])
+print(fruit[0:-3])
+print(fruit[-1:len(fruit)-3])
+print(fruit[-3:-1])
