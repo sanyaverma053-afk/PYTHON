c1326fccd6632ed2faaa31e00c7c8fb44b7e9f4d
SANYA VERMA <sanyaverma053@gmail.com>
2026-03-14 01:16:36 +0530

    stringmethods.py

---
+++ stringmethods.py
@@ -0,0 +1,10 @@
+
+a="harry"
+print(len(a))
+print(a.upper())
+print(a.lower())
+b="harry!!!!!!!!!!!!"
+print(b.rstrip("!"))
+c="harry  !!!!! harry !!"
+print(c.replace("harry","john"))
+print(c.split(" "))
