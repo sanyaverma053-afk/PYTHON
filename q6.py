ae3a480a0882884cf55d97404fe323718647c3f2
SANYA VERMA <sanyaverma053@gmail.com>
2026-03-17 00:13:45 +0530

    q6.py

---
+++ q6.py
@@ -0,0 +1,15 @@
+
+#question 6
+#Input three numbers and print the largest
+
+a=int(input("enter 1st number"))
+b=int(input("enter 2nd number"))
+c=int(input("enter 3rd number"))
+if a>b:
+    print(" a is greater than b")
+elif b>c:
+    print("b is greater than c")
+elif c>a:
+    print("c is greater than a")
+else:
+    print("invalid")
