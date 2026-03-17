91410895e12b7413d85f3d9c0d232a610a70732b
SANYA VERMA <sanyaverma053@gmail.com>
2026-03-18 00:02:35 +0530

    q7.py

---
+++ q7.py
@@ -0,0 +1,9 @@
+
+#question 7
+#Check if a year is Leap Year or not.
+
+a=int(input("enter a year"))
+if a%4==0 or a%400==0 and a%100!=0:
+    print("leap year")
+else:
+    print("not a leap year")
