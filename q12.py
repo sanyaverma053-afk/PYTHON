4d5381b5026f24843017e8cd71a38af6b8a37748
SANYA VERMA <sanyaverma053@gmail.com>
2026-03-20 01:05:27 +0530

    q12.py

---
+++ q12.py
@@ -0,0 +1,16 @@
+
+#question 12
+#Write a program to check whether a number is Armstrong Number.
+a = int(input("Enter a number: "))
+sum = 0
+temp = a
+
+while temp > 0:
+    digit = temp % 10
+    sum += digit ** 3   
+    temp //= 10
+
+if sum == a:
+    print(a, "is an Armstrong number")
+else:
+    print(a, "is not an Armstrong number")
