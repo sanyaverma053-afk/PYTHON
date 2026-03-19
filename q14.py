df6e843677d993baa8db360d4768405ae0db9b7b
SANYA VERMA <sanyaverma053@gmail.com>
2026-03-20 01:09:18 +0530

    Create q14.py

---
+++ q14.py
@@ -0,0 +1,11 @@
+
+#Q.Check if entered character is alphabet, digit or special character
+
+ch = input("Enter a character: ")
+
+if 'a' <= ch <= 'z' or 'A' <= ch <= 'Z':
+    print("Alphabet")
+elif '0' <= ch <= '9':
+    print("Digit")
+else:
+    print("Special Character")
