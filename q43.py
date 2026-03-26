8733e048209fd68e1fadc78f4c5767e1c6b4c986
SANYA VERMA <sanyaverma053@gmail.com>
2026-03-26 13:53:36 +0530

    q43.py

---
+++ q43.py
@@ -0,0 +1,8 @@
+
+#Q. Write a program to take 3 numbers as input and print their average
+
+a=int(input("enter 1st number"))
+b=int(input("enter 2nd number "))
+c=int(input("enter 3rd number"))
+avg=a*b*c/3
+print(avg)
