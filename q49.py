b7655596ccb99f98c2768fb03a050cd7c4f40ad5
SANYA VERMA <sanyaverma053@gmail.com>
2026-03-27 08:29:05 +0530

    q49.py

---
+++ q49.py
@@ -0,0 +1,19 @@
+
+# Q. Write a program that takes two numbers and prints:
+# Their sum
+# Their difference
+# Their product
+# Their division (in float)
+a=int(input("enter a number"))
+b=int(input("enter another number"))
+sum=a+b
+diff=a-b
+mul=a*b
+c=float(a)
+d=float(b)
+div=c/d
+print(sum)
+print(diff)
+print(mul)
+print(div)
+
