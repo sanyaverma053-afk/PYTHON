f820b9f3ee2f5e5b1ed2d85a44f89dafaf627662
SANYA VERMA <sanyaverma053@gmail.com>
2026-04-05 16:56:44 +0530

    Delete q46.py

--- q46.py
+++
@@ -1,8 +0,0 @@
-
-
-# Q. Write a program to input name, age, and city from the user and print them in one line. 
-
-name=input("enter name")
-age=int(input("enter age"))
-city=input("enter city")
-print("name:",name,"|age:",age, "|city:",city)
