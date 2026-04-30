53a42821b5b0473198ae107a4c3e70bf7ec53524
SANYA VERMA <sanyaverma053@gmail.com>
2026-04-30 21:38:52 +0530

    Add number positivity check in q1.py

---
+++ q1.py
@@ -0,0 +1,10 @@
+#question 1
+#Check whether a number is positive or negative.
+
+a=int(input("enter a number:"))
+if a>0:
+    print("positive")
+elif a<0:
+    print("negative")
+else:
+    print("don't know"
