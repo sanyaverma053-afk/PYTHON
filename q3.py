9d37342940f501759822ba6e69d9a6b247a37ee9
SANYA VERMA <sanyaverma053@gmail.com>
2026-05-02 23:06:33 +0530

    Delete q3.py

--- q3.py
+++
@@ -1,9 +0,0 @@
-
-#question 3
-#Input age and check if the person is eligible to vote (18+)
-
-a=int(input("enter age"))
-if a>18:
-    print("eligible to vote")
-else:
-    print("not eligible to vote")
