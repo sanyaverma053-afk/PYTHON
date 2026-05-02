ca3fd96023dc1902adad1b09ca9df8b462f82d71
SANYA VERMA <sanyaverma053@gmail.com>
2026-05-02 23:06:54 +0530

    Add age check for voting eligibility

---
+++ q3.py
@@ -0,0 +1,9 @@
+
+#question 3
+#Input age and check if the person is eligible to vote (18+)
+
+a=int(input("enter age"))
+if a>18:
+    print("eligible to vote")
+else:
+    print("not eligible to vote")
