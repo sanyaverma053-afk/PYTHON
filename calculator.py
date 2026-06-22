3e80b25426003986feb4727d8e7d1b5bb7754273
SANYA VERMA <sanyaverma053@gmail.com>
2026-06-22 22:53:50 +0530

    Add basic calculator functionality
    Implement basic arithmetic operations in a calculator.

---
+++ calculator.py
@@ -0,0 +1,12 @@
+#create a calculator
+
+a=int(input("enter a number"))
+b=int(input("enter another number"))
+sum=a+b
+diff=a-b
+mul=a*b
+div=a/b
+print("the value of a and b is", sum)
+print(diff)
+print(mul)
+print(div)
